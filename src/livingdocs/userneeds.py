ed52ec6321eedb42b676b8d85693f5e072e5b709
Espen Westgaard <espenwest@gmail.com>
2023-01-06 08:19:10 +0100

    parse files with both tab and comma separators

--- src/livingdocs/userneeds.py
+++ src/livingdocs/userneeds.py
@@ -14,4 +14,10 @@ def load_userneeds_from_file(filename):
             if len(line) != EXPECTED_NUMBER_OF_USERNEED_COLUMNS:
                 continue
             userneeds.append({"id": line[0], "description": line[1]})
+    with open(filename) as csv_file:
+        next(csv_file)  # Skip the title row
+        for line in csv.reader(csv_file, dialect="excel"):
+            if len(line) != EXPECTED_NUMBER_OF_USERNEED_COLUMNS:
+                continue
+            userneeds.append({"id": line[0], "description": line[1]})
     return userneeds
