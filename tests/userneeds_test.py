29fe3910bfe44ce8cba5471e935eee8aa484cd1b
Espen Westgaard <espenwest@gmail.com>
2023-01-06 08:19:50 +0100

    Add tests

---
+++ tests/userneeds_test.py
@@ -0,0 +1,10 @@
+"""Test parsing from file."""
+import pytest
+
+from livingdocs.userneeds import load_userneeds_from_file
+
+
+@pytest.mark.integtest
+def test_userneeds_from_file() -> None:
+    userneeds = load_userneeds_from_file("testfiles/userneeds.csv")
+    assert len(userneeds) == 6
